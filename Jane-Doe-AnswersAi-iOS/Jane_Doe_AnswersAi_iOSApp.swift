225540cb8b0c744fe65e31f1016387c8680a8925
Abhijeet Gajjar <gajjarabhijeet@gmail.com>
2024-12-12 19:20:40 -0800

    Initial Commit

---
+++ Jane-Doe-AnswersAi-iOS/Jane_Doe_AnswersAi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jane_Doe_AnswersAi_iOSApp.swift
+//  Jane-Doe-AnswersAi-iOS
+//
+//  Created by Abhijeet gajjar on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Jane_Doe_AnswersAi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
